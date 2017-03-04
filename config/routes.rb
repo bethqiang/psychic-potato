034e489dc8605be20402051b474b21e75dc3b40e
Beth Qiang <bethany.qiang@gmail.com>
2017-03-04 09:25:54 -0600

    Add google font, Bootstrap & about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # Then, Rails checks the controller file, finds the home method,
   # and says I need to gather the corresponding view file
   root to: 'pages#home'
+  # When a client hits /about, Rails will check the controller file &
+  # find the about method
+  get 'about', to: 'pages#about'
 end
