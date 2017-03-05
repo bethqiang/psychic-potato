32cc9ca40211958f29e27d5e72b24f4fe522bf05
Beth Qiang <bethany.qiang@gmail.com>
2017-03-05 14:50:13 -0600

    Add basic & pro buttons & edit individual forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+  # GET request for / (home page)
   def home
+    # Plan.find is basically Sequelize's Model.findById(id)
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
 
   def about
