d1b58455824562f9ee52af5236ed191f991f2b72
Beth Qiang <bethany.qiang@gmail.com>
2017-03-09 21:35:35 -0600

    Update flash msg & make users pick plan on signup
    - Style flash messages & add fade out.
    - If a user attempts to hit plan=3 or some nonsense like that, app will force user to pick either 1 or 2.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Only for a new action (when someone is trying to sign up), run the select_plan function
+  before_action :select_plan, only: :new
+
   def create
     # Extend default Devise gem behavior so that users signing up with the Pro account
     # save with a special Stripe subscription function
@@ -14,4 +17,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+
+  # In case someone attempts to type plan=3 or some nonsense like that into the URL
+  # private -> when you don't plan on using the function outside the file
+  private
+    def select_plan
+      unless(params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
